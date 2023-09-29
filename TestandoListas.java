c0022eaf9953d0ce8cb29799a75253b856a351c0
jotajeff <jotajeff@gmail.com>
2023-09-29 15:04:50 -0300

    novo arquivo de array

---
+++ TestandoListas.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class TestandoListas{
+
+	public static void main(String[]args){
+	String aula1 = "aprendendo Python";
+	String aula2 = "curso de Servidores";
+	String aula3 = "iniciando em MVC";
+	
+	ArrayList<String> aulas = new ArrayList<>();
+	aulas.add(aula1);
+	aulas.add(aula2);
+	aulas.add(aula3);
+	
+	System.out.println(aulas);
+	}
+	
+
+
+}
